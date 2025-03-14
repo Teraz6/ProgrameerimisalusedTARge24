346036ba12b41e18d5b3d7832c56dbeaef730a20
Teraz6 <retenk@gmail.com>
2025-03-14 15:23:15 +0200

    Add capsulation.py exercise

--- ObjectOrientedProgramming/04-Capsulation/CapsulationExe.py
+++ ObjectOrientedProgramming/04-Capsulation/CapsulationExe.py
@@ -4,4 +4,29 @@
 class Student:
     """Represent student with name, id and status."""
 
-    pass
+    def __init__(self, name: str, student_id: int):
+        """Set private attributes."""
+        self.__name = name
+        self.__id = student_id
+        self.__status = "Active"
+
+    def get_id(self) -> int:
+        """Get student id."""
+        return self.__id
+
+    def set_name(self, name: str):
+        """Set student name."""
+        self.__name = name
+
+    def get_name(self) -> str:
+        """Get student name."""
+        return self.__name
+
+    def set_status(self, status: str):
+        """Set student status."""
+        if status in {"Active", "Expelled", "Finished", "Inactive"}:
+            self.__status = status
+
+    def get_status(self):
+        """Get student status."""
+        return self.__status
